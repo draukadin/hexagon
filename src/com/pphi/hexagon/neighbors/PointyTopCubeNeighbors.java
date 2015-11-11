aee2fc321fa976c511d1f3e9277a0ae68533529e
draukadin <draukadin@gmail.com>
2015-11-11 05:59:28 -0800

    Adding Neighbors for PointyTopCube Coordinate

---
+++ src/com/pphi/hexagon/neighbors/PointyTopCubeNeighbors.java
@@ -0,0 +1,50 @@
+package com.pphi.hexagon.neighbors;
+
+import com.pphi.hexagon.coordinates.HexagonCubeCoordinate;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+public enum PointyTopCubeNeighbors {
+
+    SW(-1, 0, 1),
+    NW(0, 1, -1),
+    SE(0, -1, 1),
+    NE(1, 0, -1),
+    E(1, -1, 0),
+    W(-1, 1, 0);
+
+    private int x;
+    private int y;
+    private int z;
+
+    PointyTopCubeNeighbors(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static List<HexagonCubeCoordinate> getNeighbors(HexagonCubeCoordinate start) {
+        List<HexagonCubeCoordinate> neighbors = newArrayList();
+        int xStart = start.getX();
+        int yStart = start.getY();
+        int zStart = start.getZ();
+        for (PointyTopCubeNeighbors ptcn : PointyTopCubeNeighbors.values()) {
+            neighbors.add(new HexagonCubeCoordinate((xStart + ptcn.getX()),
+                    (yStart + ptcn.getY()), (zStart + ptcn.getZ())));
+        }
+        return neighbors;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+}
